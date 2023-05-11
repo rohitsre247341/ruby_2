d45971c90af6207edd4fc04ef88487bdb428860a
Rohit <rohitessapur@gmail.com>
2023-05-11 10:57:05 +0530

    new

--- module.rb
+++ module.rb
@@ -1,1 +1,17 @@
-puts "welcome"
+class Reading_para
+  def initialize
+    @file = File.open("new.txt")
+  end
+  def first_1
+    s = @file.read
+    puts s
+    @file.close
+  end
+  def first_2
+    s = @file.readlines.map(&:chomp)
+    p s
+    @file.close
+  end
+end
+obj = Reading_para.new
+obj.first_1
