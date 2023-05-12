2e99395e130bf6a9a02805fef148ae1772be62a5
Rohit <rohitessapur@gmail.com>
2023-05-12 21:09:39 +0530

    updated

--- circle.rb
+++ circle.rb
@@ -13,4 +13,23 @@ class Sa_1
 end
 
 obj = Sa_1.new(4)
-obj.area_2
+
+
+module Check_1
+  def initialize(s)
+    @s = s 
+  end
+  def check_2
+    if @s%2 == 0
+      p "the given number #{@s} is := even"
+    else 
+      p "the given number #{@s} is odd "
+    end
+  end
+end
+class Check_3
+  include Check_1
+end
+obj = Check_3.new(9)
+obj.check_2
+
